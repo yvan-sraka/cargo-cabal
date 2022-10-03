f4e77ff6cb83dda9b8669fe2555ae9ebe6fa0e0b
Yvan Sraka <yvan@sraka.xyz>
2022-10-03 16:44:54 +0200

    v0.2.0
    - Write an extensive README :)
    - Rename the project `hs-pack`

--- src/hsbindgen.rs
+++ src/hsbindgen.rs
@@ -1,17 +1,18 @@
-const VERSION: &str = "0.1.1";
+const VERSION: &str = "0.2.0";
 
 pub(crate) fn generate(module: &str) -> String {
     format!(
         "# Since the only `.cabal` format parser implementation and specification live
-# in Cabal itself ... this deadly simple config file is used by `hsbindgen`
+# in Cabal itself ... this deadly simple config file is used by `hs-bindgen`
 # Rust crate to get needed data (like default exposed module name).
 
 default = \"{module}\"
 
 # There is an unlikely future where instead we have a Rust `.cabal` parser,
-# that most likely would rely under the hood on an Haskell static lib wrapper
-# of `Cabal.Parse` library. But even in this case it would be nice to know the
-# `hackage-pack` version that generated the `.cabal` file used.
+# that most likely would rely under the hood on a Haskell static lib wrapper
+# of `Cabal.Parse` or https://hackage.haskell.org/package/Cabal-syntax library.
+# But even in this case, it would be nice to know the `hs-pack` version that
+# generated the `.cabal` file used.
 
 version = \"{VERSION}\""
     )
