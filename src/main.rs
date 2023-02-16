635c5484ab30cbacc8d47adafecb7bfb53827fb1
Yvan Sraka <yvan@sraka.xyz>
2023-02-16 14:01:12 +0100

    v0.8.4
    - refactor a bit and improve workflow for better `cargo-stack`
      integration

--- src/main.rs
+++ src/main.rs
@@ -280,7 +280,7 @@
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    if let Err(e) = lib::routine(args) {
+    if let Err(e) = lib::parse_cli_args(args) {
         println!("{}{}", ansi_term::Colour::Red.bold().paint("Error: "), e);
     }
 }
